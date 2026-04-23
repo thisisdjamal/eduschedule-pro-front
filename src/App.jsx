96e41025ba3f03a0433f345a8da3babcda2ced1c
Yasmine1016 <tiemtoreedwige5@gmail.com>
2026-04-23 08:15:15 +0000

    feat: ajout de ProtecdRoute pour securiser les pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import EmploiTempsPage from './pages/EmploiTempsPage'
 import CahierTexte from './pages/CahierTexte'
 import VacationsPage from './pages/VacationsPage'
+import PointageQRPage from './pages/PointageQRPage'
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/emploi-temps" element={<EmploiTempsPage />} />
-          <Route path="/cahiers" element={<CahierTexte />} />
-          <Route path="/vacations" element={<VacationsPage />} />
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          
+          <Route path="/dashboard" element={
+            <ProtectedRoute><DashboardPage /></ProtectedRoute>
+          } />
+          <Route path="/emploi-temps" element={
+            <ProtectedRoute><EmploiTempsPage /></ProtectedRoute>
+          } />
+          <Route path="/cahiers" element={
+            <ProtectedRoute><CahierTexte /></ProtectedRoute>
+          } />
+          <Route path="/vacations" element={
+            <ProtectedRoute><VacationsPage /></ProtectedRoute>
+          } />
+          <Route path="/pointage-qr" element={
+            <ProtectedRoute><PointageQRPage /></ProtectedRoute>
+          } />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
